fdfd46129601597752d56b47e38ae991dc4f4e5e
gustavog <chudijunk@gmail.com>
2014-07-12 22:12:18 +0200

    wip

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -8,14 +8,57 @@
  * Controller of the catenaApp
  */
 angular.module('catenaApp')
-  .controller('DeviceCtrl', function ($scope) {
+    .factory('Keys', ['$resource', function($resource){
+        var host = 'https://192.168.0.111';
+        var urlApi = '/keys';
+        urlApi = host + urlApi;
+        var resource = $resource(urlApi, {}, {'get': { method: 'GET', isArray: true }}  );
+        return resource;
+    }])
+    .factory('Toggle', ['$resource', function($resource){
+        var host = 'https://192.168.0.111';
+        var urlApi = '/keys';
+        urlApi = host + urlApi;
+        var resource = $resource(urlApi, {}, {'get': { method: 'GET', isArray: true }}  );
+        return resource;
+    }])
+  .controller('DeviceCtrl', function ($scope, Keys, Toggle) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
-    $scope.hello = "yeia";
-  });
+    $scope.hello = 'yeia';
+    console.log('helloooo');
+
+    var host = "http://192.168.0.111";
+    var route = "/keys";
+    Keys.get(function(results){
+        var key = results[0];
+        results.push(angular.copy(key));
+        results.push(angular.copy(key));
+        results.push(angular.copy(key));
+        results.push(angular.copy(key));
+        results.push(angular.copy(key));
+        results.push(angular.copy(key));
+        $scope.keys = results;
+        
+    });
+
+    $scope.toggleAllow = function(key){
+        if(key.status){
+            key.status = 0;
+        }else{
+            key.status = 1;
+        }
+        Toggle.get({status:key.status}, function(res){
+            
+        
+        });
+        
+        
+    };  
+  })
   .controller('AboutCtrl', function ($scope) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
@@ -23,3 +66,4 @@ angular.module('catenaApp')
       'Karma'
     ];
   });
+
